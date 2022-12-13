4b3ee489a8408a81a6f8827cd9061430f1d68312
horpeazy <iyamuhope.nosa@gmail.com>
2022-12-12 19:39:45 -0800

    feat: dpeth of a tree

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the depth of a ndeo in a binary tree
+ * @tree: pointer to the node to measure it's depth
+ * Return: depth of the node
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	int depth;
+
+	if (!tree || !tree->parent) return (0);
+
+	depth = binary_tree_depth(tree->parent);
+	return depth + 1;
+}
