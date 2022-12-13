09bf22993a1dcd753059b5b5d6f05cc14a7edecb
horpeazy <iyamuhope.nosa@gmail.com>
2022-12-12 17:15:09 -0800

    feat: create binary tree node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a node of a binary tree
+ * @parent: pointer to the parent node
+ * @value: value of the new node
+ * Return: pointer to the new node created
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	node = malloc(sizeof(binary_tree_t));
+
+	if (node == NULL)
+		return (NULL);
+
+	node->parent = parent;
+	node->n = value;
+
+	return (node);
+}
