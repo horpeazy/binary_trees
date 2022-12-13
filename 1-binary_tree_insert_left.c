5e76b118c2369ee354150d06c6f0e89028e15194
horpeazy <iyamuhope.nosa@gmail.com>
2022-12-12 17:49:15 -0800

    feat: update left node

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts a node to the left of a node
+ * @parent: parent node
+ * @value: value of new node
+ * Return: pointer to the new node and NULL on failure
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+	binary_tree_t *new;
+
+	node = malloc(sizeof(binary_tree_t));
+	new = malloc(sizeof(binary_tree_t));
+	if (node == NULL || parent == NULL || new == NULL)
+		return (NULL);
+
+	node->n = value;
+	node->parent = parent;
+
+	if (parent->left)
+	{
+		new->n = parent->left->n;
+		new->parent = node;
+		new->left = parent->left->left;
+		new->right = parent->left->right;
+		free(parent->left);
+		node->left = new;
+	}
+	parent->left = node;
+
+	return (node);
+}
