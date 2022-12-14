c0c5f514a0a1e190f9d1e3916cd8d789d0795284
horpeazy <iyamuhope.nosa@gmail.com>
2022-12-14 01:14:49 -0800

    feat: two correct

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	node->right = NULL;
 	node->left = parent->left;
 	if (parent->left)
-		parent->left->parent = node
+		parent->left->parent = node;
 	parent->left = node;
 
 	return (node);
