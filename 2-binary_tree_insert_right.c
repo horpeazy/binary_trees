3930b810dc8547ab0c402e3999c363fc574dea79
horpeazy <iyamuhope.nosa@gmail.com>
2022-12-14 01:19:50 -0800

    feat: three correct

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,25 +9,19 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
-	binary_tree_t *new;
 
+	if (!parent)
+		return (NULL);
 	node = malloc(sizeof(binary_tree_t));
-	new = malloc(sizeof(binary_tree_t));
-	if (node == NULL || parent == NULL || new == NULL)
+	if (node == NULL)
 		return (NULL);
 
 	node->n = value;
 	node->parent = parent;
-
+	node->right = parent->right;
+	node->left = NULL;
 	if (parent->right)
-	{
-		new->n = parent->right->n;
-		new->parent = node;
-		new->left = parent->right->left;
-		new->right = parent->right->right;
-		free(parent->right);
-		node->right = new;
-	}
+		parent->right->parent = node;
 	parent->right = node;
 
 	return (node);
