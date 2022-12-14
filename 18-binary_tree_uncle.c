eb8d41a0fe190822ea9e1642fc0220ec7fcc090d
horpeazy <iyamuhope.nosa@gmail.com>
2022-12-13 18:14:15 -0800

    feat: node uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - find the sibling of a node
+ * @node: pointer to the node
+ * Return: pointer to the sibling
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return node->parent->right;
+	return node->parent->left;
+}
+
+/**
+ * binary_tree_uncle - return the uncle of a node
+ * @node: pointer to node
+ * Return: uncle of node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node)
+		return (NULL);
+	return (binary_tree_sibling(node->parent));
+}
