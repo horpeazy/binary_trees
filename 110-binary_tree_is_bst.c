1fec6bc904435cffb681e6da52a331f39f412444
horpeazy <iyamuhope.nosa@gmail.com>
2022-12-15 18:51:26 -0800

    fet: 103

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -9,17 +9,17 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 	{
-		return 0;
+		return (0);
 	}
 
 	if (tree->left != NULL && tree->n <= tree->left->n)
 	{
-		return 0;
+		return (0);
 	}
 
 	if (tree->right != NULL && tree->n > tree->right->n)
 	{
-	return 0;
+	return (0);
 	}
 
 	if (!binary_tree_is_bst(tree->left) || !binary_tree_is_bst(tree->right))
